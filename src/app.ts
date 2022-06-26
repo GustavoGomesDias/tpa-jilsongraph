ef5ae58562add05f04b428025013a855a6a18ef1
GustavoGomesDias <treeelevengg@gmail.com>
2022-06-26 16:23:19 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 import docRoute from './routes/document';
 import nodeRoute from './routes/node';
 import edgeRoute from './routes/edge';
 
+// const whitelist = ['http://example1.com', 'http://example2.com'];
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: '*',
+  preflightContinue: false,
+};
+
 const app: Express = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors(options));
 
 // Routes
 app.use('/doc', docRoute);
